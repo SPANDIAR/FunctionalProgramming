a84d160518dc48241cfc018099e40cb5f87a383e
sankar_pandiarajan <spandiarajan@apple.com>
2020-01-03 15:43:53 -0800

    enhanced to include Even check

--- FuncProg-Simple-01/src/io/spandiar/fp01/PrintListUsingFP.java
+++ FuncProg-Simple-01/src/io/spandiar/fp01/PrintListUsingFP.java
@@ -5,8 +5,22 @@ import java.util.List;
 public class PrintListUsingFP {
 
 	public static void main(String[] args) {
-		List<Integer> intList = List.of(5, 6, 8, 12, 55, 63, 6, 13);
-		printListElementsFunctionally(intList);
+		List<Integer> intList = List.of(5, 6, 8, 12, 55, 63, 6, 13, 24);
+		//printListElementsFunctionally(intList);
+		printEvenNosInList(intList);
+	}
+
+	private static void printEvenNosInList(List<Integer> intList) {
+		
+		intList.stream()
+				.distinct()
+				.filter(PrintListUsingFP::isEven)
+				.forEach(System.out::println);
+				
+	}
+	
+	private static boolean isEven(int number){
+		return (number %2 ==0);
 	}
 
 	private static void printListElementsFunctionally(List<Integer> intList) {
