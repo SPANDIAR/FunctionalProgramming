86f0fc0b16e647d0300d3a8a8d393c7f3f2687f2
sankar_pandiarajan <spandiarajan@apple.com>
2020-01-03 15:31:29 -0800

    initial commit

---
+++ FuncProg-Simple-01/src/io/spandiar/fp01/PrintListUsingFP.java
@@ -0,0 +1,21 @@
+package io.spandiar.fp01;
+
+import java.util.List;
+
+public class PrintListUsingFP {
+
+	public static void main(String[] args) {
+		List<Integer> intList = List.of(5, 6, 8, 12, 55, 63, 6, 13);
+		printListElementsFunctionally(intList);
+	}
+
+	private static void printListElementsFunctionally(List<Integer> intList) {
+		intList.stream()
+				.forEach(PrintListUsingFP::printList);
+	}
+	
+	private static void printList(int number){
+		System.out.println(number);
+	}
+
+}
